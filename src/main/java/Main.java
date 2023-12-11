fd4d3d854333d117f8f92efa9df511a8b10641f5
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-12-11 18:04:56 -0500

    put parsers together, started printToFile

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,10 @@ import org.apache.commons.io.IOUtils;
 import java.io.IOException;
 import java.util.*;
 import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -13,27 +17,10 @@ public class Main {
 
     public static void main(String[] args) throws Exception{
         String output = (new Main()).readRawDataToString();
-        System.out.println(output);
-    }
+        Map<String, List<String>> map = mainParse(output);
+        printToFile();
+        //System.out.println(output);
 
-    public ArrayList<String> miniParse(String item) throws Exception {
-        String regex = "[$&+,;=?@#|'<>.^*()%!-]";
-        Pattern pattern = Pattern.compile(regex);
-        String[] temp = pattern.split(item.toLowerCase());
-        ArrayList<String> itemList = new ArrayList<>();
-//        for(String items: temp){
-//            String patternString = "(item)";
-//            Pattern patternTemp = pattern.compile(item);
-//            Matcher matcher = patternTemp.matcher(items);
-//        }
-        // temp[0] = {name: milk}
-        Pattern newPattern = Pattern.compile(":(.+)");
-        for(String items: temp){
-            String[] newTemp = newPattern.split(items);
-            itemList.add(newTemp[0]);
-            //System.out.println(output);
-
-        }return itemList;
     }
 
 
@@ -91,7 +78,24 @@ public class Main {
             String output = String.format(formattedStringName, names[i], times[i]);
             System.out.println(output);
         }
-
     }
 
+    public ArrayList<String> miniParse(String item) throws Exception {
+        String regex = "[$&+,;=?@#|'<>.^*()%!-]";
+        Pattern pattern = Pattern.compile(regex);
+        String[] temp = pattern.split(item.toLowerCase());
+        ArrayList<String> itemList = new ArrayList<>();
+//        for(String items: temp){
+//            String patternString = "(item)";
+//            Pattern patternTemp = pattern.compile(item);
+//            Matcher matcher = patternTemp.matcher(items);
+//        }
+        // temp[0] = {name: milk}
+        Pattern newPattern = Pattern.compile(":(.+)");
+        for(String items: temp){
+            String[] newTemp = newPattern.split(items);
+            itemList.add(newTemp[0]);
+        }
+        return itemList;
+    }
 }
