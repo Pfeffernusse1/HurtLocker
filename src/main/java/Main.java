7660d228477656235a5d94354e7fbd34ca5b5195
Loc <loc@Zipcoders-MacBook-Pro-3.local>
2023-12-11 17:02:11 -0500

    resolve merge conflicts

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,18 +3,37 @@ import java.io.IOException;
 import java.util.*;
 import java.util.regex.Pattern;
 
-public class Main  {
+public class Main {
 
-    public String readRawDataToString() throws Exception {
+    public String readRawDataToString() throws Exception{
         ClassLoader classLoader = getClass().getClassLoader();
         String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
         return result;
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         String output = (new Main()).readRawDataToString();
         System.out.println(output);
+    }
 
+    public ArrayList<String> miniParse(String item) throws Exception {
+        String regex = "[$&+,;=?@#|'<>.^*()%!-]";
+        Pattern pattern = Pattern.compile(regex);
+        String[] temp = pattern.split(item.toLowerCase());
+        ArrayList<String> itemList = new ArrayList<>();
+//        for(String items: temp){
+//            String patternString = "(item)";
+//            Pattern patternTemp = pattern.compile(item);
+//            Matcher matcher = patternTemp.matcher(items);
+//        }
+        // temp[0] = {name: milk}
+        Pattern newPattern = Pattern.compile(":(.+)");
+        for (String items : temp) {
+            String[] newTemp = newPattern.split(items);
+            itemList.add(newTemp[0]);
+            //System.out.println(output);
+
+        }return itemList;
     }
 
 
@@ -72,6 +91,7 @@ public class Main  {
             String output = String.format(formattedStringName, names[i], times[i]);
             System.out.println(output);
         }
+
     }
 
 }
